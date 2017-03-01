19dac635474e160f132c63bb72e43ac325050763
charleslapeyre <lapeyrecc@gmail.com>
2017-03-01 08:35:04 -0600

    add hint to use constant in location controller to create drop-down list of cities in ATL

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,6 +2,9 @@ class Location < ApplicationRecord
   geocoded_by :full_street_address
   after_validation :geocode
 
+  # use constant in your model to relay a group of values that will never change
+  # MARTA_CITIES = []
+
   def full_street_address
     "#{street_address}, #{city}, GA"
   end
